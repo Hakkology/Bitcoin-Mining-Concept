23b943c5bd0bf2d53a6d51e3468b72333028ac1b
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-05 23:45:41 +0300

    Add files via upload

---
+++ Hakan.ConsoleApp.BtcMine/Hash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Hakan.ConsoleApp.BtcMine
+{
+    public class Hash
+    {
+        public static string GetHashString(string Hsh)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes($"{Hsh}");
+                byte[] outputBytes = sha256.ComputeHash(inputBytes);
+                return Convert.ToBase64String(outputBytes);
+            }
+        }
+
+    }
+}
---
+++ Hakan.ConsoleApp.BtcMine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Timers;
+
+namespace Hakan.ConsoleApp.BtcMine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int n = 4;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            string FirstInput = "My name is Earl";
+            Random rand = new Random();
+            int Nonce = rand.Next(0,7);
+            string Hashx = Hash.GetHashString(FirstInput);
+            Console.WriteLine($"Hash value is: {Hashx}");
+
+            do
+            {
+                Hashx = Hash.GetHashString($"{Hashx}+{Nonce}");
+                count++;
+            } while (Hashx.Substring(0, n) != String.Concat(Enumerable.Repeat("0",n)));
+
+            watch.Stop();
+            Console.WriteLine($"Current Hash value is {Hashx}");
+            Console.WriteLine($"This iteration took {count} times");
+            Console.WriteLine($"It  took {watch.ElapsedMilliseconds/1000} seconds to reach desired conditions.");
+            Console.ReadLine();
+        }
+
+
+    }
+
+}
+
